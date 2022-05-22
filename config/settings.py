9008a816452698ca7018fb570eecfe14fc3d68fb
quake <kkornilov1337@gmail.com>
2022-05-22 20:34:31 +0500

    create scanner file and settings

---
+++ config/settings.py
@@ -0,0 +1,28 @@
+import logging
+
+PORT = 8000
+HOST = '127.0.0.1'
+CHECKING_TIMEOUT = 0.5
+
+LOG_SETTINGS = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(module)s %(message)s'
+        },
+    },
+    'handlers': {
+        'stdout': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+        'loggers': {
+            '': {
+                'handlers': ['stdout'],
+                'level': logging.DEBUG,
+                'propagate': True,
+            },
+        }
+    }
+}
---
+++ scanner.py
@@ -0,0 +1,29 @@
+import logging
+import socket
+import config
+
+log = logging.getLogger()
+
+
+class BadIpError(Exception):
+    pass
+
+
+def scan_port(ip: str, port: int) -> bool:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(config.CHECKING_TIMEOUT)
+    try:
+        result = sock.connect_ex((ip, port))
+        if result == 0:
+            log.debug(f'Port: {port} state: open')
+            return True
+        else:
+            log.debug(f'Port: {port} state: close')
+            sock.close()
+            return False
+    except socket.gaierror:
+        log.debug(f'Incorrect ip {ip}')
+        raise BadIpError
+    except Exception as e:
+        log.error(f'Error on check port: {e}')
+        raise
